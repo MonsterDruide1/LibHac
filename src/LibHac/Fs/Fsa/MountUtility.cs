60bf791404f62ac4564193a04140241a0b5c8c2c
Alex Barney <thealexbarney@gmail.com>
2022-05-17 02:26:26 -0700

    Fix an off-by-one error when parsing mount names

--- src/LibHac/Fs/Fsa/MountUtility.cs
+++ src/LibHac/Fs/Fsa/MountUtility.cs
@@ -47,7 +47,7 @@ public static class MountUtility
             return Result.Success;
         }
 
-        for (int i = 0; i <= maxMountLen; i++)
+        for (int i = 0; i < maxMountLen; i++)
         {
             if (path[i] == DriveSeparator)
             {
